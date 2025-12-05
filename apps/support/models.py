d66ebaf0a0c825b8662dc0c4e4c76eae1e580e74
Lego developer <papuridalego@gmail.com>
2025-12-05 22:23:28 +0330

    Add faqs page and snippets

--- apps/support/models.py
+++ apps/support/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-from wagtail.models import Page
+from wagtail.models import Page, Orderable
 from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel, InlinePanel
 from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
+from wagtail.snippets.models import register_snippet
 from modelcluster.fields import ParentalKey
 
 
@@ -21,6 +22,45 @@ class ContactPage(AbstractEmailForm):
     ]
 
 
+@register_snippet
+class FAQCategory(models.Model):
+    name = models.CharField(max_length=100)
+    order = models.IntegerField(default=0)
+    
+    panels = [
+        FieldPanel('name'),
+        FieldPanel('order'),
+    ]
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = 'FAQ Categories'
+        ordering = ['order', 'name']
+
+
+@register_snippet
+class FAQ(models.Model):
+    category = models.ForeignKey(FAQCategory, on_delete=models.CASCADE, related_name='faqs')
+    question = models.CharField(max_length=500)
+    answer = RichTextField()
+    order = models.IntegerField(default=0)
+    
+    panels = [
+        FieldPanel('category'),
+        FieldPanel('question'),
+        FieldPanel('answer'),
+        FieldPanel('order'),
+    ]
+    
+    def __str__(self):
+        return self.question
+    
+    class Meta:
+        ordering = ['order', 'question']
+
+
 class FAQPage(Page):
     """FAQ page"""
     intro = RichTextField(blank=True)
@@ -28,3 +68,9 @@ class FAQPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('intro'),
     ]
+    
+    def get_context(self, request):
+        context = super().get_context(request)
+        categories = FAQCategory.objects.prefetch_related('faqs').all()
+        context['faq_categories'] = categories
+        return context
